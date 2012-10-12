bf5333630bc1308e0f5a2e14dbe566bd0e1d0db9
Caio Romão <caioromao@gmail.com>
2012-10-12 22:24:08 +0200

    Deopinionate a bit more

--- bashd.bash
+++ bashd.bash
@@ -18,10 +18,6 @@ fi
 bashd_source ${BASHD_DIR}/lib/*
 bashd_source ${BASHD_DIR}/custom/*
 
-export EDITOR=vim
-export BROWSER=chromium
-export PYTHONSTARTUP="${HOME}/.pythonrc.py"
-
 pathprepend ~/bin
 pathprepend ~/.bin
 pathprepend ~/.local/bin
@@ -31,19 +27,6 @@ shopt -s checkwinsize
 shopt -s extglob
 shopt -s no_empty_cmd_completion
 
-alias mv='mv -i'
-alias cp='cp -i'
-alias rm='rm -I'
-alias j='jobs'
-alias grep='egrep'
-alias ls='ls --color -h --group-directories-first'
-alias l='ls -lh'
-alias la='ls -lhA'
-alias a='ls -d .*'
-alias -- -='cd -'
-alias beep="echo -ne '\a'"
-alias p='ps -eo pid,ruser,cmd| grep -i'
-
 # Save multiline commands as single-line
 shopt -s cmdhist
 # Append to history instead of overwriting
@@ -51,10 +34,7 @@ shopt -s histappend
 
 export HISTSIZE=40000
 export HISTFILESIZE=40000
-export HISTIGNORE="ls:l:clear:d:cd:bg:fg:history"
 export HISTCONTROL="erasedups:ignorespace"
-export HISTTIMEFORMAT='[%Y-%m-%d %H:%M]  '
-# }}}
 
 # avoid carrying over test statuses
 true
