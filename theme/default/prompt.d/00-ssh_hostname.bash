2d80d22ec9303e16d8e5dbff391aeb0618eef783
Caio Romão <caioromao@gmail.com>
2013-03-30 16:24:52 +0100

    Detect ssh connections through dropbear

--- theme/default/prompt.d/00-ssh_hostname.bash
+++ theme/default/prompt.d/00-ssh_hostname.bash
@@ -2,6 +2,9 @@ __prompt_ssh_hostname() {
     if [ -n "$SSH_CLIENT" ]; then
         bashd_prompt_append "${FG_BOLD_GREEN}$(hostname -s) "
     fi
+    if [[ $SSH_AUTH_SOCK =~ dropbear ]]; then
+        bashd_prompt_append "${FG_BOLD_RED}$(hostname -s) "
+    fi
 }
 
 bashd_prompt_register __prompt_ssh_hostname
