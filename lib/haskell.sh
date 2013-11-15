75de41a241cf000beaa073b21a1222839bbe95af
Caio Romão <caioromao@gmail.com>
2013-11-15 12:24:49 -0200

    Remove some unused libs

--- lib/haskell.sh
+++
@@ -1,8 +0,0 @@
-__load_cabal() {
-    local cabal_dir="${HOME}/.cabal"
-    test ! -d ${cabal_dir} && return 0
-    pathprepend ${cabal_dir}/bin/
-    export MANPATH=${cabal_dir}/share/man/:$(manpath -g)
-}
-
-__load_cabal
